cd8c748ec59f7a06eb7622d89fe4e0d8cadd7dee
Spirobel <spirobel@protonmail.com>
2024-05-05 02:35:24 +0800

    update version

--- backend/backend.ts
+++ backend/backend.ts
@@ -1,12 +1,12 @@
-import { url, head, html } from "@spirobel/mininext";
-head(html` <title>hello hello</title> `);
+import { url, head, html, commonHead } from "@spirobel/mininext";
+head(html`<title>hello hello</title>${commonHead}`);
 url.set([
   ["/", (mini) => mini.html`<h1>Hello world</h1>`],
   [
     "/bye",
     (mini) =>
       mini.html`<h1>Goodbye world</h1>${mini.head(
-        mini.html` <title>bye bye</title>`
+        mini.html`<title>bye bye</title>${commonHead}`
       )}`,
   ],
 ]);
