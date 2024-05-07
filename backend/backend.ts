993b7c948fe2d2ed982fa0b3937df5f404cf17d9
Spirobel <spirobel@protonmail.com>
2024-05-07 23:03:56 +0800

    extract login endpoint

--- backend/backend.ts
+++ backend/backend.ts
@@ -1,52 +1,7 @@
 import { url, head, commonHead, cssReset, Mini } from "@spirobel/mininext";
 import { solanaWalletStyles } from "./styling/solanaWalletStyles";
-import { parse } from "cookie";
-import { db } from "../db/db";
-import { sessionCookies } from "../db/schema";
-import { eq } from "drizzle-orm";
-import type { SolanaSignInInput } from "@solana/wallet-standard-features";
-import { verifySignIn } from "@solana/wallet-standard-util";
-import bs58 from "bs58";
-import { z } from "zod";
-export const CheckChallengeRequest = z.object({
-  input: z.object({
-    address: z.string().min(20).max(50),
-  }),
-  output: z.object({
-    signature: z.string(),
-    signedMessage: z.string(),
-  }),
-});
+import { logoutEndpoint, verifyLoginEndpoint } from "./user/login";
 
-export type CheckChallengeRequestType = z.infer<typeof CheckChallengeRequest>;
-export function getSessionId(req: Request) {
-  return parse(req.headers.get("Cookie") || "")["sessionId"] || null;
-}
-export async function deleteCookie(req: Request) {
-  const sessionId = getSessionId(req);
-  if (!sessionId) return;
-  await db.transaction(async (tx) => {
-    const cookie = (
-      await tx
-        .select()
-        .from(sessionCookies)
-        .where(eq(sessionCookies.cookie, sessionId))
-    )[0];
-    if (!cookie.address) return;
-    await tx
-      .delete(sessionCookies)
-      .where(eq(sessionCookies.address, cookie.address));
-  });
-}
-
-export async function createSession(address: string) {
-  const uuid = crypto.randomUUID();
-
-  // Insert a new sessionCookie with the generated UUID as the cookie value for the given address
-  await db.insert(sessionCookies).values({ cookie: uuid, address });
-
-  return uuid;
-}
 const loginScriptTag = url.frontend("/login/Login.tsx", solanaWalletStyles);
 head((mini) => mini.html`<title>hello hello</title>${commonHead}${cssReset}`);
 const navbar = (mini: Mini) => mini.html`
@@ -122,54 +77,8 @@ url.set([
       </div>${loginScriptTag}`;
     },
   ],
-  [
-    "/login/verifySignInMessage",
-    url.postJson(async (mini) => {
-      const parsedData = CheckChallengeRequest.safeParse(mini.form.formJson);
-      if (!parsedData.success) {
-        return mini.json`{"success":false}`;
-      }
-      const data = parsedData.data;
-      const domain = mini.requrl.host;
-      const address = data.input.address;
-      const statement =
-        "Clicking Sign or Approve only means you have proved this wallet is owned by you. This request will not trigger any blockchain transaction or cost any gas fee.";
-      const signInData: SolanaSignInInput = {
-        address,
-        domain,
-        statement,
-      };
-      const publicKey = bs58.decode(data.input.address);
-      const signedMessage = bs58.decode(data.output.signedMessage);
-      const signature = bs58.decode(data.output.signature);
-      const success = verifySignIn(signInData, {
-        signedMessage,
-        signature,
-        // @ts-ignore
-        account: { publicKey },
-      });
-      if (success) {
-        return mini.json`${{ success }}${mini.headers({
-          "Content-Type": "application/json; charset=utf-8",
-          "Set-Cookie": `sessionId=${await createSession(
-            address
-          )}; expires=Fri, 31 Dec 9999 23:59:59 GMT; Secure; HttpOnly; SameSite=Strict; path=/`,
-        })}`;
-      }
-      return mini.json`${{ success }}`;
-    }),
-  ],
-  [
-    "/logout",
-    url.postJson(async (mini) => {
-      console.log(mini.form.formJson);
-      await deleteCookie(mini.req);
-      return mini.json`{"loggedOut": true}${mini.headers({
-        "Content-Type": "application/json; charset=utf-8",
-        "Set-Cookie": `sessionId==deleted; expires=Thu, 01 Jan 1970 00:00:00 GMT Secure; HttpOnly; SameSite=Strict; path=/`,
-      })}`;
-    }),
-  ],
+  ["/login/verifySignInMessage", verifyLoginEndpoint],
+  ["/logout", logoutEndpoint],
 ]);
 
 export default url.install;
